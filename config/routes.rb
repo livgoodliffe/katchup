9d04b6bfb8440dc60e6999ad6976a4ddd54819bb
Joel James <jayfiled@gmail.com>
2019-06-03 16:47:24 +1000

    fix feed routes and add feed page

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,5 @@ Rails.application.routes.draw do
 
   resources :spots, only: [:show, :index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :feed, only: [:index]
+  resources :feeds, only: [:index]
 end
