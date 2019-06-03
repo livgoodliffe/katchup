189b9705ed61e0de54d8ad75f8f5e043ec64dac3
Joel James <jayfiled@gmail.com>
2019-06-03 15:18:30 +1000

    fix catchup migration

--- db/migrate/20190603050411_create_catchups.rb
+++ db/migrate/20190603050411_create_catchups.rb
@@ -3,7 +3,7 @@ class CreateCatchups < ActiveRecord::Migration[5.2]
     create_table :catchups do |t|
       t.references :user, foreign_key: true
       t.references :spot, foreign_key: true
-      t.DateTime :time
+      t.datetime :time
 
       t.timestamps
     end
