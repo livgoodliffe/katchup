9acd855d1154a61ca7e1add80246dd869ec790e1
Daniel Troy <daniel_j_troy@hotmail.com>
2019-06-04 11:49:19 +1000

    search in lists

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -2,5 +2,15 @@ class ListsController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index
+    # byebug
+    if params[:search].present?
+      search = params[:search]
+      @spots = Spot.where('name ILIKE ?', "%#{search}%")
+      respond_to do |format|
+        format.js
+      end
+    else
+      @spots = Spot.all
+    end
   end
 end
