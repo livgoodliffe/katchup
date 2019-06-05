785524b540682e13d05ddb84bfa752f750885d2d
Joel James <jayfiled@gmail.com>
2019-06-05 12:31:11 +1000

    add friendship model, add controller methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,9 @@ class User < ApplicationRecord
   has_many :friend_requests, dependent: :destroy
   has_many :pending_friends, through: :friend_requests, source: :friend
 
+  has_many :friendships, dependent: :destroy
+  has_many :friends, through: :friendships
+
   has_many :spots
   has_many :favourites
   has_many :wishlists
