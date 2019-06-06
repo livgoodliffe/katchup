da34b03edf56a502fcece84dff3680d10c5ec7bf
Joel James <jayfiled@gmail.com>
2019-06-06 16:51:13 +1000

    populate feed cards with data

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ApplicationRecord
   include PublicActivity::Model
-  tracked
+  tracked owner: Proc.new{ |controller, model| controller.current_user }
 
   belongs_to :user
   belongs_to :spot
