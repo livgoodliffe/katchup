fad51ebf261b2e06934c1acb8708e7e149e173ff
Daniel Troy <daniel_j_troy@hotmail.com>
2019-06-06 18:59:28 +1000

    WIP..

--- app/controllers/catchups_controller.rb
+++ app/controllers/catchups_controller.rb
@@ -6,11 +6,18 @@ class CatchupsController < ApplicationController
 
   def new
     @friends = current_user.friends
+    favourite_spots = current_user.favourite_spots.map { |spot| spot }
+    wishlist_spots = current_user.wishlist_spots.map { |spot| spot }
+    @spots = favourite_spots.concat(wishlist_spots)
+
   end
 
   def create
     # create catchup
     # create guests
+    puts params
+    # puts
 
+    render 'catchups/index'
   end
 end
