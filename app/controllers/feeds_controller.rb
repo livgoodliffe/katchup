da27257faefd53b73315f7514f5ea8f38fa26f4a
Joel James <jayfiled@gmail.com>
2019-06-07 10:56:39 +1000

    filter out destroyed feed items, wishlist and favourites

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -3,7 +3,11 @@ class FeedsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
 
   def index
-    @activities = PublicActivity::Activity.order("created_at desc").where(owner_id: current_user.friends)
+    # byebug
+    @activities = PublicActivity::Activity
+                  .order("created_at desc")
+                  .where(owner_id: current_user.friends)
+                  .where("key ilike ?", "%create%")
     # byebug
   end
 end
