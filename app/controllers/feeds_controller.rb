513503e1856b47c46fe6fceb65bafb7214ae465a
Olivia Goodliffe <ocgoodliffe@gmail.com>
2019-06-09 00:56:02 +1000

    search from feed now very smooth you dont have to click again

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -3,11 +3,20 @@ class FeedsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
 
   def index
-    @spots = Spot.all
-
     @activities = PublicActivity::Activity
                   .order("created_at desc")
                   .where(owner_id: current_user.friends)
                   .where("key ilike ?", "%create%")
+
+    if params[:search].present?
+      search = params[:search]
+      @spots = Spot.where('name ILIKE ?', "%#{search}%")
+      respond_to do |format|
+        format.js
+      end
+    else
+      @spots = Spot.all
+    end
   end
+
 end
