aacb8d21af659c1840d044ebfe337bf2089c60a7
Olivia Goodliffe <ocgoodliffe@gmail.com>
2019-06-09 09:42:11 +1000

    if statements added to javascript

--- app/javascript/feed/index.js
+++ app/javascript/feed/index.js
@@ -1,38 +1,44 @@
 const slideInSearch = () => {
   const feed = document.querySelector('#feed-page');
-  const search_select = document.querySelector('#search-search form');
+  const searchSelect = document.querySelector('#search-search form');
 
-  const search_page = document.querySelector('#search-page');
+  const searchPage = document.querySelector('#search-page');
   const back = document.querySelector('#search-back-button');
 
-  const map_page = document.querySelector('#feed-map');
-  const map_icon = document.querySelector('#map-icon');
+  const mapPage = document.querySelector('#feed-map');
+  const mapIcon = document.querySelector('#map-icon');
 
 
-    search_select.addEventListener('click', () => {
-      search_page.classList.add('animated', 'slideInRight');
-      search_page.classList.remove('hidden');
+  if (searchSelect) {
+    searchSelect.addEventListener('click', () => {
+      searchPage.classList.add('animated', 'slideInRight');
+      searchPage.classList.remove('hidden');
 
       feed.classList.add('hidden');
       back.classList.add('fas', 'fa-arrow-left', 'red-arrow', 'animated', 'slideInLeft');
     });
+  };
 
+  if (back) {
     back.addEventListener('click', () => {
-      search_page.classList.add('animated', 'slideOutRight');
-      search_page.classList.remove('slideOutRight');
+      searchPage.classList.add('animated', 'slideOutRight');
+      searchPage.classList.remove('slideOutRight');
 
       back.classList.remove('fas', 'fa-arrow-left', 'red-arrow', 'slideInLeft');
-      search_page.classList.add('hidden');
+      searchPage.classList.add('hidden');
       feed.classList.remove('hidden');
-    })
+    });
+  };
 
-    map_icon.addEventListener('click', () => {
-      map_page.classList.add('animated', 'slideInRight');
-      map_page.classList.remove('hidden');
+  if (mapIcon) {
+    mapIcon.addEventListener('click', () => {
+      mapPage.classList.add('animated', 'slideInRight');
+      mapPage.classList.remove('hidden');
 
       feed.classList.add('hidden');
       back.classList.add('fas', 'fa-arrow-left', 'red-arrow', 'animated', 'slideInLeft');
-    })
+    });
+  };
 };
 
 export { slideInSearch };
