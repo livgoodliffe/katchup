887b80347fe226d957f61c2788eb2c2c14501c8f
Olivia Goodliffe <ocgoodliffe@gmail.com>
2019-06-08 23:01:10 +1000

    search slider quicker

--- app/javascript/feed/index.js
+++ app/javascript/feed/index.js
@@ -1,9 +1,10 @@
 const slideInSearch = () => {
   const feed = document.querySelector('#feed-page');
+  const feed_select = document.querySelector('#feed-page-selector');
   const search = document.querySelector('#search-page');
   const back = document.querySelector('#search-back-button');
 
-    feed.addEventListener('click', () => {
+    feed_select.addEventListener('click', () => {
       search.classList.add('animated', 'slideInRight');
       feed.classList.add('hidden')
       search.classList.remove('hidden');
