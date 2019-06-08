c2c297d564198bf3d4344a7c50432626c50ddc45
Daniel Troy <daniel_j_troy@hotmail.com>
2019-06-08 17:59:07 +1000

    review stars in form submit and in listing

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,9 +5,11 @@ import searchListener from '../search';
 import geoLocation from '../geolocation';
 import catchupListeners from '../catchup';
 import flashFadeOut from '../flash_fade_out';
+import reviewStars from '../review_stars';
 
 initMapbox();
 searchListener();
 geoLocation();
 catchupListeners();
 flashFadeOut();
+reviewStars();
