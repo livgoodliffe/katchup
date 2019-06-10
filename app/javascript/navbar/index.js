fcec9972ec4468c9ee7e23e4781c3e24c4b4e15c
Olivia Goodliffe <ocgoodliffe@gmail.com>
2019-06-10 14:56:15 +1000

    new icon database added and in navbar

---
+++ app/javascript/navbar/index.js
@@ -0,0 +1,10 @@
+const openNav = () => {
+  document.getElementById("mySidenav").style.width = "250px";
+};
+
+const closeNav = () => {
+  document.getElementById("mySidenav").style.width = "0";
+};
+
+window.openNav = openNav;
+window.closeNav = closeNav;
