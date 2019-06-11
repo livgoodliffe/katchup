50b3903e02a0f89cbcb1ec3e5e2e398e9fbfc65b
Daniel Troy <daniel_j_troy@hotmail.com>
2019-06-11 15:28:05 +1000

    katchup update

--- app/channels/catchup_channel.rb
+++ app/channels/catchup_channel.rb
@@ -6,4 +6,16 @@ class CatchupChannel < ApplicationCable::Channel
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
   end
+
+  def receive(data)
+    return unless data["request"] == "list"
+
+    puts "received a list request"
+    Notification.where(user_id: current_user.id).each do |notification|
+      puts "going to send this as message to catchup#{current_user.id} JSON.parse(notification.content)"
+      if notification.dismissed == false
+        ActionCable.server.broadcast("catchup#{current_user.id}", JSON.parse(notification.content))
+      end
+    end
+  end
 end
--- app/javascript/channels/catchup_channel.js
+++ app/javascript/channels/catchup_channel.js
@@ -1,30 +1,58 @@
-/* global App */
+/* global App, Rails */
+const catchupInviteForm = document.getElementById('catchup-invitation-form');
+const catchupInvitationId = document.getElementById('catchup_invitation_id');
+const receivedCatchupInvitationIds = [];
+
+const displayNavbarBadge = () => {
+  document.getElementById('catchup-navbar-badge').classList.remove('hidden');
+  // App.catchupChannel.send({ message: 'sending back from client' });
+};
 
 export default () => {
   console.log('catchup client side websockets');
-  App.cable.subscriptions.create({ channel: 'CatchupChannel', room: `catchup` }, {
-    received(data) {
-      document.getElementById('catchup-navbar-badge').classList.remove('hidden');
 
-      console.log('received data', data);
-      this.appendLine(data);
-    },
+  App.catchupChannel = App.cable.subscriptions.create({
+    channel: 'CatchupChannel',
+    room: 'catchup',
+  }, {
 
-    appendLine(data) {
-      const html = this.createLine(data);
-      // const element = document.querySelector("[data-chat-room='Best Room']");
-      const element = document.getElementById('catchup-chat-messages');
-      if (element) {
-        element.insertAdjacentHTML('beforeend', html);
+    connected() {
+      if (catchupInviteForm) {
+        const sendListRequest = () => this.send({ request: 'list' });
+        sendListRequest();
+        setInterval(sendListRequest, 5000);
       }
     },
 
-    createLine(data) {
-      return `
-      <article class="chat-line">
-        <span class="message">${data.message}</span>
-      </article>
-    `;
+    received(data) {
+      const { catchup_id: catchupId } = data;
+
+      console.log('received data', data);
+
+      if (data.message === 'alert') {
+        displayNavbarBadge();
+      } else if (catchupInviteForm && !receivedCatchupInvitationIds.includes(catchupId)) {
+        receivedCatchupInvitationIds.push(catchupId);
+        catchupInvitationId.value = catchupId;
+        Rails.fire(catchupInviteForm, 'submit');
+      }
+      // this.appendLine(data);
     },
+
+    // appendLine(data) {
+    //   const html = this.createLine(data);
+    //   const element = document.getElementById('catchup-invites');
+    //   if (element) {
+    //     element.insertAdjacentHTML('beforeend', html);
+    //   }
+    // },
+
+    // createLine(data) {
+    //   return `
+    //   <article class="chat-line">
+    //     <span class="message">You have been invited to catchup with id ${data.catchup_id}</span>
+    //   </article>
+    // `;
+    // },
   });
 };
