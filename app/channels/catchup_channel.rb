2ad7b40904e0cf0d5dc44ecf6501c657bdfe6087
Daniel Troy <daniel_j_troy@hotmail.com>
2019-06-11 16:26:36 +1000

    remove puts

--- app/channels/catchup_channel.rb
+++ app/channels/catchup_channel.rb
@@ -10,9 +10,7 @@ class CatchupChannel < ApplicationCable::Channel
   def receive(data)
     return unless data["request"] == "list"
 
-    puts "received a list request"
     Notification.where(user_id: current_user.id).each do |notification|
-      puts "going to send this as message to catchup#{current_user.id} JSON.parse(notification.content)"
       if notification.dismissed == false && notification.catchup?
         ActionCable.server.broadcast("catchup#{current_user.id}", JSON.parse(notification.content))
       end
