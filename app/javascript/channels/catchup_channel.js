4aafcac56196b92da21c12d7787f1672ff0e0de8
Daniel Troy <daniel_j_troy@hotmail.com>
2019-06-12 17:23:39 +1000

    catchup header badges

--- app/javascript/channels/catchup_channel.js
+++ app/javascript/channels/catchup_channel.js
@@ -1,15 +1,36 @@
 /* global App, Rails */
+const catchUpPage = document.getElementById('catchup-page');
 const catchupInviteForm = document.getElementById('catchup-invitation-form');
 const catchupInvitationId = document.getElementById('catchup_invitation_id');
 const guestResponseForm = document.getElementById('guest-response-form');
 const guestResponseId = document.getElementById('guest_response_id');
+
+const myCatchupBadge = document.getElementById('my-catchup-badge');
+const otherCatchupBadge = document.getElementById('other-catchup-badge');
+const navbarCatchupBadge = document.getElementById('navbar-catchup-badge');
+
 const receivedCatchupInvitationIds = [];
 const receivedGuestUpdateIds = [];
 
-const displayNavbarBadge = () => {
-  const catchupNavbarBadge = document.getElementById('catchup-navbar-badge');
-  if (catchupNavbarBadge) {
-    catchupNavbarBadge.classList.remove('hidden');
+const displayBadge = (type) => {
+  switch (type) {
+    case 'navbar':
+      if (navbarCatchupBadge) {
+        navbarCatchupBadge.classList.remove('hidden');
+      }
+      break;
+    case 'mine':
+      if (myCatchupBadge) {
+        myCatchupBadge.classList.remove('hidden');
+      }
+      break;
+    case 'other':
+      if (otherCatchupBadge) {
+        otherCatchupBadge.classList.remove('hidden');
+      }
+      break;
+    default:
+      break;
   }
 };
 
@@ -26,19 +47,19 @@ export default () => {
     },
 
     received(data) {
-      displayNavbarBadge();
+      displayBadge('navbar');
       const { guest_id: guestId, catchup_id: catchupId } = data;
 
       if (catchupId !== undefined) {
+        if (catchUpPage) displayBadge('other');
         if (catchupInviteForm && !receivedCatchupInvitationIds.includes(catchupId)) {
-          console.log(`received catchup invite for catchup_id ${catchupId} (should only see once per reload)`);
           receivedCatchupInvitationIds.push(catchupId);
           catchupInvitationId.value = catchupId;
           Rails.fire(catchupInviteForm, 'submit'); // get catchup invite card
         }
       } else if (guestId !== undefined) {
+        if (catchUpPage) displayBadge('mine');
         if (guestResponseForm && !receivedGuestUpdateIds.includes(guestId)) {
-          console.log(`received guest status update for guest_id ${guestId} (should only see once per reload)`);
           receivedGuestUpdateIds.push(guestId);
           guestResponseId.value = guestId;
           Rails.fire(guestResponseForm, 'submit'); // get guest response card
