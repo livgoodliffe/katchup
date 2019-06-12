4866a4fd058b0cebe4637df0ca3a0392586a93f0
Joel James <jayfiled@gmail.com>
2019-06-13 09:22:14 +1000

    add an if statement to delete if spot is on wishlist

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -10,8 +10,9 @@ class FavouritesController < ApplicationController
   end
 
   def create
-    # byebug
     @spot = Spot.find(params[:spot_id])
+    # remove from wishlist if you are adding it to your 'ive been there' list
+    current_user.wishlists.destroy(@spot) if in_wishlist?(@spot)
     current_user.favourites.create(spot: @spot)
 
     respond_to do |format|
@@ -43,5 +44,9 @@ class FavouritesController < ApplicationController
 
   def favourite_params
     params.require(:favourite).permit(:spot_id)
+  end
+
+  def in_wishlist?(spot)
+    current_user.wishlists.include?(spot)
   end
 end
