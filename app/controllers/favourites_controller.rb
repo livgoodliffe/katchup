2b5dd09daf29c8f560c37b2f2af1c3182e80afac
Joel James <jayfiled@gmail.com>
2019-06-13 12:40:11 +1000

    remove from wishlist if you have been there toggle

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -10,9 +10,10 @@ class FavouritesController < ApplicationController
   end
 
   def create
+    # byebug
     @spot = Spot.find(params[:spot_id])
     # remove from wishlist if you are adding it to your 'ive been there' list
-    current_user.wishlists.destroy(@spot) if in_wishlist?(@spot)
+    current_user.wishlists.destroy(in_wishlist?(@spot)) if in_wishlist?(@spot)
     current_user.favourites.create(spot: @spot)
 
     respond_to do |format|
@@ -47,6 +48,6 @@ class FavouritesController < ApplicationController
   end
 
   def in_wishlist?(spot)
-    current_user.wishlists.include?(spot)
+    current_user.wishlists.find_by spot_id: spot[:id]
   end
 end
