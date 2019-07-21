358a125c4f7c72a15e2f407a89d1f3a246f259db
Olivia Goodliffe <ocgoodliffe@gmail.com>
2019-07-21 13:32:21 +1000

    user params in users controller updated

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -26,6 +26,6 @@ class Api::V1::UsersController < Api::V1::BaseController
   end
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    params.permit(:first_name, :last_name, :email, :password, :password_confirmation)
   end
 end
