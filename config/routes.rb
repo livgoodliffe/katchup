6fa283b407fccd4d572ac42286f0bf93021f7036
Olivia Goodliffe <ocgoodliffe@gmail.com>
2019-10-09 09:34:20 +1100

    added reviews to api

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :spots, only: [ :index ]
+      resources :reviews, only: [:index ]
     end
   end
 
