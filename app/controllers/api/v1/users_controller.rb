c95419739d72c5c1b875fbdc6693fcc1f1cc9d19
Olivia Goodliffe <ocgoodliffe@gmail.com>
2019-10-09 12:34:13 +1100

    user sign up added

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -9,4 +9,21 @@ class Api::V1::UsersController < Api::V1::BaseController
     authorize @user
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render text: @user.authentication_token, status: 201
+    else
+      render json: @user.errors, status: 422
+    end
+
+    # do i have to authorise or use pundit
+  end
+
+  private
+
+  def user_params
+      params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    end
+
 end
