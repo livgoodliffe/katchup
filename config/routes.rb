68cab259f222386d91e587d03810e868cb030ec3
Olivia Goodliffe <ocgoodliffe@gmail.com>
2019-10-09 12:37:50 +1100

    add create to users api

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :spots, only: [ :index, :show ]
       resources :reviews, only: [:index, :show, :update ]
-      resources :users, only: [ :index, :show ]
+      resources :users, only: [ :index, :show, :create ]
     end
   end
 
