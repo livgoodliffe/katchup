9d4afe1e56e7d64c66e3550b119f65690d6f6222
Olivia Goodliffe <ocgoodliffe@gmail.com>
2019-10-09 16:37:47 +1100

    user create returns token

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -11,19 +11,21 @@ class Api::V1::UsersController < Api::V1::BaseController
 
   def create
     @user = User.new(user_params)
+
     if @user.save
-      render text: @user.authentication_token, status: 201
+      render json: @user.as_json(only: [:authentication_token]), status: :created
     else
-      render json: @user.errors, status: 422
+      head(:unprocessable_entity)
     end
 
-    # do i have to authorise or use pundit
+    authorize @user
+
   end
 
   private
 
   def user_params
-      params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+      params.permit(:first_name, :last_name, :email, :password, :password_confirmation)
     end
 
 end
--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -8,4 +8,8 @@ class UserPolicy < ApplicationPolicy
   def show?
     true
   end
+
+   def create?
+    true
+  end
 end
