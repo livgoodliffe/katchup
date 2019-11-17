e0a46dac92695e65b538c2f78d2308a47c2b05a3
Olivia Goodliffe <ocgoodliffe@gmail.com>
2019-11-18 10:28:43 +1100

    remove pwa

--- app/controllers/api/v1/spots_controller.rb
+++ app/controllers/api/v1/spots_controller.rb
@@ -1,19 +1,28 @@
 class Api::V1::SpotsController < Api::V1::BaseController
-
+  # before_action :set_spot, only: [ :show ]
   skip_before_action :authenticate_user!
 
-  def index
-    @spots = Spot.all
 
+  def index   def index
+    @spots = policy_scope(Spot)     @spots = Spot.all
+    # makes sure pundit is being used
     render json: @spots
 
+  end   end
+
+
+  def show    def show
+    @spot = Spot.find(params[:id])      @spot = Spot.find(params[:id])
+    authorize @spot     authorize @spot
   end
 
-  def show
-    @spot = Spot.find(params[:id])
-    authorize @spot
 
-    @review = Review.new
+  # private     @review = Review.new
   end
 
+
+  # def set_spot
+  #   @spot = Spot.find(params[:id])
+  #   authorize @spot  # For Pundit - dont need this because all users can see details on all spots?
+  # end
 end
