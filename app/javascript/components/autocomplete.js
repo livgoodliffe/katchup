31332580d2fb7f2244c37b0d0454a461a5c5f8d1
Olivia Goodliffe <ocgoodliffe@gmail.com>
2019-11-18 15:08:05 +1100

    google maps

---
+++ app/javascript/components/autocomplete.js
@@ -0,0 +1,17 @@
+// app/javascript/components/autocomplete.js
+function autocomplete() {
+  document.addEventListener("DOMContentLoaded", function() {
+    var flatAddress = document.getElementById('flat_address');
+
+    if (flatAddress) {
+      var autocomplete = new google.maps.places.Autocomplete(flatAddress, { types: [ 'geocode' ] });
+      google.maps.event.addDomListener(flatAddress, 'keydown', function(e) {
+        if (e.key === "Enter") {
+          e.preventDefault(); // Do not submit the form on Enter.
+        }
+      });
+    }
+  });
+}
+
+export { autocomplete };
--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,10 +1,13 @@
 Geocoder.configure(
   # Geocoding options
   # timeout: 3,                 # geocoding service timeout (secs)
-  # lookup: :google,         # name of geocoding service (symbol)
+  lookup: :google,         # name of geocoding service (symbol)
+
+  api_key:   ENV['GOOGLE_API_SERVER_KEY'],
+
   # ip_lookup: :ipinfo_io,      # name of IP address geocoding service (symbol)
   # language: :en,              # ISO-639 language code
-  # use_https: false,           # use HTTPS for lookup requests? (if supported)
+  use_https: true,           # use HTTPS for lookup requests? (if supported)
   # http_proxy: nil,            # HTTP proxy server (user:pass@host:port)
   # https_proxy: nil,           # HTTPS proxy server (user:pass@host:port)
   # api_key: google_places_key,               # API key for geocoding service
