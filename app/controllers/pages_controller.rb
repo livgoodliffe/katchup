9c17e077568d88ca8deba84614a21e58b1e70f28
Olivia Goodliffe <ocgoodliffe@gmail.com>
2019-11-25 10:21:55 +1100

    feed search broken

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,18 +3,18 @@ class PagesController < ApplicationController
   require "optparse"
   require "http"
 
-  API_HOST = "https://api.yelp.com"
-  SEARCH_PATH = "/v3/businesses/search"
-  BUSINESS_PATH = "/v3/businesses/"  # trailing / because we append the business id to the path
+  # API_HOST = "https://api.yelp.com"
+  # SEARCH_PATH = "/v3/businesses/search"
+  # BUSINESS_PATH = "/v3/businesses/"  # trailing / because we append the business id to the path
   # API_KEY = ENV["YELP_API"]
-  SEARCH_LIMIT = 3
-
-  ZOMATO_API = ENV["ZOMATO_API"]
-
-  GP_API = ENV["GOOGLE_PLACES"]
-
+  # SEARCH_LIMIT = 3
+  # GP_API = ENV["GOOGLE_PLACES"]
 
   skip_before_action :authenticate_user!, only: [:home]
+
+  def initialize
+    @client = Romato::Zomato.new(ENV["ZOMATO_API"])
+  end
 
   def home
     render :layout => 'without_navbar'
@@ -23,43 +23,28 @@ class PagesController < ApplicationController
   def kitchen_sink
   end
 
-  def initialize
-    @client = Romato::Zomato.new(ENV["ZOMATO_API"])
-  end
-
   def search
 
     term = params[:query]
-
-    @categories = @client.get_categories
-
     results = @client.get_search( { q: term, lat: current_user.latitude, lon: current_user.longitude } )
-
     @spots = results["restaurants"]
 
     # GOOGLE PLACES WORKING SEARCH
-
     # @client = GooglePlaces::Client.new(GP_API)
-
     # @nearby = @client.spots(current_user.latitude, current_user.longitude, :types => ['restaurant', 'cafe', 'bar'])
-
     # @search = @client.spots_by_query(term, :types => ['food'])
 
     # YELP WORKING SEARCH
-
     # url = "#{API_HOST}#{SEARCH_PATH}"
-
     # params = {
     #   term: term,
     #   latitude: current_user.latitude,
     #   longitude: current_user.longitude,
     #   limit: SEARCH_LIMIT
     # }
-
     # response = HTTP.auth("Bearer #{API_KEY}").get(url, params: params)
     # @restaurants = response.parse["businesses"]
 
   end
-
 
 end
