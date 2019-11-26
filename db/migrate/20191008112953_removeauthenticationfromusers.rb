27f5f3f73338da586abf1c1d1fdbbbbc7b85896c
Sheila Leveille <sbleveille@gmail.com>
2019-11-26 12:46:34 +1100

    initial commit and set up

--- db/migrate/20191008112953_removeauthenticationfromusers.rb
+++ db/migrate/20191008112953_removeauthenticationfromusers.rb
@@ -1,5 +1,5 @@
 class Removeauthenticationfromusers < ActiveRecord::Migration[5.2]
   def change
-    remove_column :users, :authentication_token, :string
+    # remove_column :users, :authentication_token, :string
   end
 end
