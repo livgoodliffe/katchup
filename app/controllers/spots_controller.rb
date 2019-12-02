2d89bb6d5322fb6107eb949b8f4d46abda8ad4a1
Olivia Goodliffe <ocgoodliffe@gmail.com>
2019-12-02 13:06:08 +1100

    news feed search working

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -7,23 +7,30 @@ class SpotsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
-      term = params[:query]
 
-      @spots_db = Spot.where('name ILIKE ?', "%#{term}%")
+    term = params[:query]
 
-      @client = Romato::Zomato.new(ENV["ZOMATO_API"])
-      results = @client.get_search( { q: term, lat: current_user.latitude, lon: current_user.longitude } )
-      @zom_results = results["restaurants"]
-      @spots_zom = []
+    @spots_db = Spot.where('name ILIKE ?', "%#{term}%")
 
-      @zom_results.each do |spot|
-        zom_res_id = spot["restaurant"]["R"]["res_id"]
-        result = Spot.exists?(res_id: zom_res_id)
-        if result == false
-          @spots_zom.push(spot)
-        end
+    @client = Romato::Zomato.new(ENV["ZOMATO_API"])
+    results = @client.get_search( { q: term, lat: current_user.latitude, lon: current_user.longitude } )
+    @zom_results = results["restaurants"]
+    @spots_zom = []
+
+    @zom_results.each do |spot|
+      zom_res_id = spot["restaurant"]["R"]["res_id"]
+      result = Spot.exists?(res_id: zom_res_id)
+      if result == false
+        @spots_zom.push(spot)
       end
+    end
 
+    if params[:query]
+      respond_to do |format|
+        format.html
+        format.js{}
+      end
+    end
 
     # News feed
 
